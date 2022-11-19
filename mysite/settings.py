e27d43c22567f4aea136ddda1a69140cb7c966cd
Mariusz <denis.mario1992@gmail.com>
2022-11-19 14:38:53 +0100

    add host pythonanywhere

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(fdbtb5mx^nxqw3ypy%!@b69_8_k18_s1r=+)!j&#uzg3ew7ye'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','goldlife.pythonanywhere.com']
 
 
 # Application definition
